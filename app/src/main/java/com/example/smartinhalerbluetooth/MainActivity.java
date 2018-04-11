f4001ceba3691e2ee874876013d1b63ba92514ca
Rahul <rahulgupta@iiitdjmj.ac.in>
2018-04-11 12:08:47 +0530

    // comment

--- app/src/main/java/com/example/smartinhalerbluetooth/MainActivity.java
+++ app/src/main/java/com/example/smartinhalerbluetooth/MainActivity.java
@@ -58,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //hello rahul
 
         intilizeView();
     }
